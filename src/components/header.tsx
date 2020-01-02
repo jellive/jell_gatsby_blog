3a0cf092571a2593f8033835b1374cc426127efe
PD Jell <jellive7@gmail.com>
2020-01-02 11:47:04 +0900

    add scroll trigger.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,36 +2,43 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar'
+import Slide from '@material-ui/core/Slide'
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
 
-const Header = ({ siteTitle }) => (
-  <AppBar
-    // style={{
-    //   background: `rebeccapurple`,
-    //   marginBottom: `1.45rem`,
-    // }}
-    color='default'
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+const Header = ({ siteTitle }) => {
+  const trigger = useScrollTrigger();
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      <AppBar
+        // style={{
+        //   background: `rebeccapurple`,
+        //   marginBottom: `1.45rem`,
+        // }}
+        color='default'
+      >
+        <div
           style={{
-            color: `gray`,
-            textDecoration: `none`,
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `1.45rem 1.0875rem`,
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </AppBar>
-);
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: `gray`,
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+        </div>
+      </AppBar>
+    </Slide >
+  )
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
